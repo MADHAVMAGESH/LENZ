ce9fff1aa8d7da5afa8a66254a03e4807a51ac6c
MADHAVMAGESH <pmmagesh08@gmail.com>
2026-03-29 11:16:21 +0530

    Add User model class with id, email, displayName, avatar properties

---
+++ backend/src/models/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, email, displayName, avatar) {
+        this.id = id;
+        this.email = email;
+        this.displayName = displayName;
+        this.avatar = avatar;
+    }
+}
+
+module.exports = User;
